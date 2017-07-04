fba94616fd8732155d56e7193e8eb133fa586f9d
Hieu Hoang <hieuhoang@gmail.com>
2017-07-04 08:07:47 +0100

    get ready to move printer in search

--- src/amun/common/printer.h
+++ src/amun/common/printer.h
@@ -16,68 +16,8 @@ std::vector<size_t> GetAlignment(const HypothesisPtr& hypothesis);
 std::string GetAlignmentString(const std::vector<size_t>& alignment);
 std::string GetSoftAlignmentString(const HypothesisPtr& hypothesis);
 
-template <class OStream>
-void Printer(const God &god, const History& history, OStream& out) {
-  auto bestTranslation = history.Top();
-  std::vector<std::string> bestSentenceWords = god.Postprocess(god.GetTargetVocab()(bestTranslation.first));
-
-  std::string best = Join(bestSentenceWords);
-  if (god.Get<bool>("return-alignment")) {
-    best += GetAlignmentString(GetAlignment(bestTranslation.second));
-  }
-  if (god.Get<bool>("return-soft-alignment")) {
-    best += GetSoftAlignmentString(bestTranslation.second);
-  }
-
-  if (god.Get<bool>("n-best")) {
-    std::vector<std::string> scorerNames = god.GetScorerNames();
-    const NBestList &nbl = history.NBest(god.Get<size_t>("beam-size"));
-    if (god.Get<bool>("wipo")) {
-      out << "OUT: " << nbl.size() << std::endl;
-    }
-    for (size_t i = 0; i < nbl.size(); ++i) {
-      const Result& result = nbl[i];
-      const Words &words = result.first;
-      const HypothesisPtr &hypo = result.second;
-
-      if(god.Get<bool>("wipo")) {
-        out << "OUT: ";
-      }
-      std::string translation = Join(god.Postprocess(god.GetTargetVocab()(words)));
-      if (god.Get<bool>("return-alignment")) {
-        translation += GetAlignmentString(GetAlignment(bestTranslation.second));
-      }
-      out << history.GetLineNum() << " ||| " << translation << " |||";
-
-      for(size_t j = 0; j < hypo->GetCostBreakdown().size(); ++j) {
-        out << " " << scorerNames[j] << "= " << std::setprecision(3) << std::fixed << hypo->GetCostBreakdown()[j];
-      }
-
-      if(god.Get<bool>("normalize")) {
-        out << " ||| " << std::setprecision(3) << std::fixed << hypo->GetCost() / words.size();
-      }
-      else {
-        out << " ||| " << std::setprecision(3) << std::fixed << hypo->GetCost();
-      }
-
-      if(i < nbl.size() - 1)
-        out << std::endl;
-      else
-        out << std::flush;
-
-    }
-  } else {
-    out << best << std::flush;
-  }
-}
-
-template <class OStream>
-void Printer(const God &god, const Histories& histories, OStream& out) {
-  for (size_t i = 0; i < histories.size(); ++i) {
-    const History& history = *histories.at(i).get();
-    Printer(god, history, out);
-  }
-}
+void Printer(const God &god, const History& history, std::ostream& out);
+void Printer(const God &god, const Histories& histories, std::ostream& out);
 
 }
 
