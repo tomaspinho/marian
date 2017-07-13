8a5114c84d87fdb2e4add337d62863b34dd9a8c1
Hieu Hoang <hieuhoang@gmail.com>
2017-07-13 12:42:45 +0100

    debug segfault

--- src/amun/gpu/decoder/encoder_decoder.cu
+++ src/amun/gpu/decoder/encoder_decoder.cu
@@ -18,40 +18,40 @@ namespace GPU {
 
 void EncoderDecoder::DecodeAsync(const God &god)
 {
-  //return;
-
-  while (true) {
-    cerr << "DecodeAsync encDecBuffer_=" << encDecBuffer_.size() << endl;
-    mblas::EncParamsPtr encParams = encDecBuffer_.remove();
-    assert(encParams.get());
-    assert(encParams->sentences.get());
-    //cerr << "BeginSentenceState encParams->sourceContext_=" << encParams->sourceContext_.Debug(0) << endl;
-
-    try {
+  try {
+    while (true) {
+      cerr << "DecodeAsync1 encDecBuffer_=" << encDecBuffer_.size() << endl;
+      mblas::EncParamsPtr encParams = encDecBuffer_.remove();
+      cerr << "DecodeAsync2 encDecBuffer_=" << encDecBuffer_.size() << endl;
+      assert(encParams.get());
+      assert(encParams->sentences.get());
+      //cerr << "BeginSentenceState encParams->sourceContext_=" << encParams->sourceContext_.Debug(0) << endl;
+      cerr << "DecodeAsync3" << endl;
       DecodeAsync(god, encParams);
+      cerr << "DecodeAsync4" << endl;
     }
-    catch(thrust::system_error &e)
-    {
-      std::cerr << "CUDA error during some_function: " << e.what() << std::endl;
-      abort();
-    }
-    catch(std::bad_alloc &e)
-    {
-      std::cerr << "Bad memory allocation during some_function: " << e.what() << std::endl;
-      abort();
-    }
-    catch(std::runtime_error &e)
-    {
-      std::cerr << "Runtime error during some_function: " << e.what() << std::endl;
-      abort();
-    }
-    catch(...)
-    {
-      std::cerr << "Some other kind of error during some_function" << std::endl;
-      abort();
-    }
-
   }
+  catch(thrust::system_error &e)
+  {
+    std::cerr << "CUDA error during some_function: " << e.what() << std::endl;
+    abort();
+  }
+  catch(std::bad_alloc &e)
+  {
+    std::cerr << "Bad memory allocation during some_function: " << e.what() << std::endl;
+    abort();
+  }
+  catch(std::runtime_error &e)
+  {
+    std::cerr << "Runtime error during some_function: " << e.what() << std::endl;
+    abort();
+  }
+  catch(...)
+  {
+    std::cerr << "Some other kind of error during some_function" << std::endl;
+    abort();
+  }
+
 }
 
 void EncoderDecoder::Decode(const God &god)
@@ -156,7 +156,9 @@ EncoderDecoder::EncoderDecoder(
 
 EncoderDecoder::~EncoderDecoder()
 {
+  cerr << "~EncoderDecoder1" << endl;
   decThread_->join();
+  cerr << "~EncoderDecoder2" << endl;
 }
 
 State* EncoderDecoder::NewState() const {
@@ -171,8 +173,9 @@ void EncoderDecoder::Encode(const SentencesPtr source) {
 
   encoder_->Encode(*source, tab_, encParams);
 
-  cerr << "Encode encDecBuffer_=" << encDecBuffer_.size() << endl;
+  cerr << "Encode1 encDecBuffer_=" << encDecBuffer_.size() << endl;
   encDecBuffer_.add(encParams);
+  cerr << "Encode2 encDecBuffer_=" << encDecBuffer_.size() << endl;
   //cerr << "Encode encParams->sourceContext_=" << encParams->sourceContext_.Debug(0) << endl;
 
   PAUSE_TIMER("Encode");
