b67a869efdd7e5d22a26d6647567a1ff6d1fc54d
Hieu Hoang <hieuhoang@gmail.com>
2017-07-13 16:43:00 +0100

    move methods to .cpp

--- src/amun/common/history.cpp
+++ src/amun/common/history.cpp
@@ -11,6 +11,42 @@ History::History(size_t lineNo, bool normalizeScore, size_t maxLength)
   Add({HypothesisPtr(new Hypothesis())});
 }
 
+void History::Add(const Beam& beam) {
+  if (beam.back()->GetPrevHyp() != nullptr) {
+    for (size_t j = 0; j < beam.size(); ++j)
+      if(beam[j]->GetWord() == EOS_ID || size() == maxLength_ ) {
+        float cost = normalize_ ? beam[j]->GetCost() / history_.size() : beam[j]->GetCost();
+        topHyps_.push({ history_.size(), j, cost });
+      }
+  }
+  history_.push_back(beam);
+}
+
+NBestList History::NBest(size_t n) const {
+  NBestList nbest;
+  auto topHypsCopy = topHyps_;
+  while (nbest.size() < n && !topHypsCopy.empty()) {
+    auto bestHypCoord = topHypsCopy.top();
+    topHypsCopy.pop();
+
+    size_t start = bestHypCoord.i;
+    size_t j  = bestHypCoord.j;
+
+    Words targetWords;
+    HypothesisPtr bestHyp = history_[start][j];
+    while(bestHyp->GetPrevHyp() != nullptr) {
+      targetWords.push_back(bestHyp->GetWord());
+      bestHyp = bestHyp->GetPrevHyp();
+    }
+
+    std::reverse(targetWords.begin(), targetWords.end());
+    nbest.emplace_back(targetWords, history_[bestHypCoord.i][bestHypCoord.j]);
+  }
+  return nbest;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 Histories::Histories(const Sentences& sentences, bool normalizeScore)
  : coll_(sentences.size())
